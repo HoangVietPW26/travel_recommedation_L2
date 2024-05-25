f025c501d68cecf027f8a64cb7d9ec51bbee9dc8
Hoang <nhviet26@gmail.com>
2024-05-25 14:13:53 +0700

    feat: setup configuartion

---
+++ src/config.py
@@ -0,0 +1,20 @@
+from os import environ
+from dotenv import dotenv_values
+
+env_values = {
+    **dotenv_values,
+    **environ
+}
+
+class Configuration:
+    
+    """
+        App configuration.
+
+        Notes: if the variable is required, access using env_values['VARIABLE_NAME']
+        to make the runtime error show up sooner, otherwise provide default value using
+        env_values.get('VARIABLE_NAME', VALUE)
+    """
+
+    APP_HOST = env_values.get("APP_HOST", "127.0.0.1")
+    APP_PORT = env_values.get("APP_PORT", 3000)
